ac891f3b036c033a6b628a672a3199be0df8f636
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2022-10-30 14:50:16 -0500

    Update recomend_app.py

--- recomend_app.py
+++ recomend_app.py
@@ -3,37 +3,37 @@ import pandas as pd
 import numpy as np
 import string
 import pickle
-df1 = pd.read_csv('Webtoon Dataset.csv',error_bad_lines=False, engine ='python')
+df1 = pd.read_csv('dice_com-job_us_sample.csv',error_bad_lines=False, engine ='python')
 df1 = df1.dropna()
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 tdif = TfidfVectorizer(stop_words='english')
 
-df1['Summary'] = df1['Summary'].fillna('')
+df1['jobdescription'] = df1['jobdescription'].fillna('')
 
-tdif_matrix = tdif.fit_transform(df1['Summary'])
+tdif_matrix = tdif.fit_transform(df1['jobdescription'])
 
 tdif_matrix.shape
 
 from sklearn.metrics.pairwise import sigmoid_kernel
 
 cosine_sim = sigmoid_kernel(tdif_matrix, tdif_matrix)
-indices = pd.Series(df1.index, index=df1['Name']).drop_duplicates()
+indices = pd.Series(df1.index, index=df1['jobtitle']).drop_duplicates()
 def get_recommendations(title, cosine_sim=cosine_sim):
   idx = indices[title]
   sim_scores = list(enumerate(cosine_sim[idx]))
   sim_scores = sorted(sim_scores, key=lambda X: X[1], reverse=True)
   sim_scores = sim_scores[1:16]
   tech_indices = [i[0] for i in sim_scores]
-  return df1['Name'].iloc[tech_indices]
+  return df1['jobtitle'].iloc[tech_indices]
 
 
 
-st.header('Webtoon Comics Recommender')
+st.header('tech jobs recommender')
 movies = pickle.load(open('job_list.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-toon_list = movies['Name'].values
+toon_list = movies['jobtitle'].values
 selected_toon = st.selectbox(
     "Type or select a Webtoon from the dropdown",
     toon_list
